29aacadee69d15bf2a8b38c15be3b6d409e70a7a
AlexFangSW <alexfangsw@gmail.com>
2024-01-31 22:49:51 +0800

    git blame

--- init.lua
+++ init.lua
@@ -2,9 +2,8 @@
 - [ok] see telescope keymaps
 - [ok] use lazygit/
 - git fugitive ?
-  - [ok] preserve line changes of previous git plugin
-- [ok] retain old yank after past
-- [ok] file tree/ or just use netrw
+- [ok] preserve line changes of previous git plugin
+- [ok] retain old yank after past [ok] file tree/ or just use netrw
 - [ok] highlight search, clear on esc esc
 - [ok] python linting + yapf formatting on Save
 - [ok] catppuccine style
@@ -141,6 +140,7 @@ require('lazy').setup({
                 topdelete = { text = '‾' },
                 changedelete = { text = '~' },
             },
+
         }
     },
 
@@ -175,15 +175,16 @@ require('lazy').setup({
         opts = {},
     },
 
-    -- "gc" to comment visual regions/lines
     {
         'numToStr/Comment.nvim',
         opts = {
             toggler = {
-                line = '<C-\\>'
+                line = '<C-\\>',
+                block = "*******"
             },
             opleader = {
-                line = '<C-\\>'
+                line = '<C-\\>',
+                block = "*******"
             }
         }
     },
@@ -338,15 +339,16 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>gh', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 
 -- [[ Highlight on yank ]]
@@ -362,6 +364,50 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure nvim-tree ]]
 require("nvim-tree").setup()
+
+-- [[ Configure gitsigns ]]
+require('gitsigns').setup {
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        -- map('n', ']c', function()
+        --     if vim.wo.diff then return ']c' end
+        --     vim.schedule(function() gs.next_hunk() end)
+        --     return '<Ignore>'
+        -- end, { expr = true })
+        --
+        -- map('n', '[c', function()
+        --     if vim.wo.diff then return '[c' end
+        --     vim.schedule(function() gs.prev_hunk() end)
+        --     return '<Ignore>'
+        -- end, { expr = true })
+
+        -- Actions
+        -- map('n', '<leader>hs', gs.stage_hunk)
+        -- map('n', '<leader>hr', gs.reset_hunk)
+        -- map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        -- map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        -- map('n', '<leader>hS', gs.stage_buffer)
+        -- map('n', '<leader>hu', gs.undo_stage_hunk)
+        -- map('n', '<leader>hR', gs.reset_buffer)
+        -- map('n', '<leader>hp', gs.preview_hunk)
+        -- map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        -- map('n', '<leader>hd', gs.diffthis)
+        -- map('n', '<leader>hD', function() gs.diffthis('~') end)
+        -- map('n', '<leader>td', gs.toggle_deleted)
+
+        -- Text object
+        -- map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
+}
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
@@ -451,15 +497,15 @@ end
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-    })
-end, { desc = '[/] Fuzzily search in current buffer' })
+-- vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+-- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+-- vim.keymap.set('n', '<leader>/', function()
+--     -- You can pass additional configuration to telescope to change theme, layout, etc.
+--     require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+--         winblend = 10,
+--         previewer = false,
+--     })
+-- end, { desc = '[/] Fuzzily search in current buffer' })
 
 local function telescope_live_grep_open_files()
     require('telescope.builtin').live_grep {
@@ -467,16 +513,16 @@ local function telescope_live_grep_open_files()
         prompt_title = 'Live Grep in Open Files',
     }
 end
-vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
-vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+-- vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
+-- vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
+-- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Search Files' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+-- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<C-f>', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+-- vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
+-- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+-- vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -520,8 +566,8 @@ vim.defer_fn(function()
                 },
             },
             move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
+                enable = false,
+                set_jumps = false, -- whether to set jumps in the jumplist
                 goto_next_start = {
                     [']m'] = '@function.outer',
                     [']]'] = '@class.outer',
@@ -540,7 +586,7 @@ vim.defer_fn(function()
                 },
             },
             swap = {
-                enable = true,
+                enable = false,
                 swap_next = {
                     ['<leader>a'] = '@parameter.inner',
                 },
@@ -570,14 +616,14 @@ local on_attach = function(_, bufnr)
     end
 
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
     nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
     nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
     local function double_hover()
         vim.lsp.buf.hover()
@@ -588,12 +634,12 @@ local on_attach = function(_, bufnr)
     -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
+    -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    -- nmap('<leader>wl', function()
+    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, '[W]orkspace [L]ist Folders')
 
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -602,23 +648,23 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-    ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-    ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-    ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-    ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-    ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-    ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-    ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-    ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-    ['<leader>'] = { name = 'VISUAL <leader>' },
-    ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
-
+-- require('which-key').register {
+--     ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+--     ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+--     ['<leader>g'] = { name = '[G]et', _ = 'which_key_ignore' },
+--     ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+--     ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+-- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+--     ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+--     ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+-- }
+-- -- register which-key VISUAL mode
+-- -- required for visual <leader>hs (hunk stage) to work
+-- require('which-key').register({
+--     ['<leader>'] = { name = 'VISUAL <leader>' },
+--     ['<leader>h'] = { 'Git [H]unk' },
+-- }, { mode = 'v' })
+--
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
@@ -692,11 +738,11 @@ cmp.setup {
         completeopt = 'menu,menuone,noinsert',
     },
     mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- ['<C-n>'] = cmp.mapping.select_next_item(),
         -- ['<C-p>'] = cmp.mapping.select_prev_item(),
         -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
+        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- ['<C-Space>'] = cmp.mapping.complete {},
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
@@ -710,15 +756,15 @@ cmp.setup {
                 fallback()
             end
         end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
+        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_prev_item()
+        --     elseif luasnip.locally_jumpable(-1) then
+        --         luasnip.jump(-1)
+        --     else
+        --         fallback()
+        --     end
+        -- end, { 'i', 's' }),
     },
     sources = {
         { name = 'nvim_lsp' },
