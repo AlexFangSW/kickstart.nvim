f5e3a0e67014e1c64c28bb37ef19911b29c96541
AlexFangSW <alexfangsw@gmail.com>
2024-04-19 10:03:56 +0800

    feat: command to enable / disable auto-format

--- lua/alexfangsw/plugins/conform.lua
+++ lua/alexfangsw/plugins/conform.lua
@@ -5,12 +5,29 @@ return {
 	config = function()
 		-- [[ Configure Conform (autoformat)]]
 		local conform = require("conform")
+
+		vim.api.nvim_create_user_command("FormatDisable", function()
+			vim.g.disable_autoformat = true
+			print("Disable autoformat-on-save")
+		end, {
+			desc = "Disable autoformat-on-save"
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.g.disable_autoformat = false
+			print("Re-enable autoformat-on-save")
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+
 		conform.setup({
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			format_on_save = function()
+				-- Disable with a global variable
+				if vim.g.disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 			formatters_by_ft = {
 				-- lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
