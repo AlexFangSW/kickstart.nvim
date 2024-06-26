38043d822956d4e06698bab6b31ac3caf069f01c
AlexFangSW <alexfangsw@gmail.com>
2024-06-26 17:56:09 +0800

    feat: command to reset indent

--- lua/alexfangsw/after/utils.lua
+++ lua/alexfangsw/after/utils.lua
@@ -2,3 +2,6 @@
 -- https://www.reddit.com/r/neovim/comments/u221as/how_can_i_copy_the_current_buffers_relative_path/
 vim.api.nvim_create_user_command("CopyRelPath", "call setreg('+', expand('%'))", {})
 vim.keymap.set("n", "<leader><leader>", ":CopyRelPath<CR>")
+
+-- reset indentation
+vim.api.nvim_create_user_command("ResetIndent", ":setlocal tabstop=2 softtabstop=2 shiftwidth=2 expandtab", {})
