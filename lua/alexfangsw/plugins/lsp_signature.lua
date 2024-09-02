5e4292b417af2f15e0dacd5167db739e47f24467
AlexFangSW <alexfangsw@gmail.com>
2024-09-02 15:54:43 +0800

    fix: lsp_signature lag
    use "hrsh7th/cmp-nvim-lsp-signature-help" as replacement

--- lua/alexfangsw/plugins/lsp_signature.lua
+++ lua/alexfangsw/plugins/lsp_signature.lua
@@ -1,12 +1,24 @@
+-- return {
+-- 	"ray-x/lsp_signature.nvim",
+-- 	event = "VeryLazy",
+-- 	config = function()
+-- 		-- [[ Configure lsp signature]]
+-- 		local lsp_signature = require("lsp_signature")
+-- 		lsp_signature.setup({
+-- 			hint_enable = false
+-- 		})
+-- 	end
+--
+-- }
 return {
-	"ray-x/lsp_signature.nvim",
+	"hrsh7th/cmp-nvim-lsp-signature-help",
 	event = "VeryLazy",
 	config = function()
-		-- [[ Configure lsp signature]]
-		local lsp_signature = require("lsp_signature")
+		local lsp_signature = require("cmp")
 		lsp_signature.setup({
-			hint_enable = false
+			sources = {
+				{ name = 'nvim_lsp_signature_help' }
+			}
 		})
 	end
-
 }
