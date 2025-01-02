d7345d2b61c1dcc1441a8a5614fe11ba44408fb2
AlexFangSW <alexfangsw@gmail.com>
2025-01-02 17:09:59 +0800

    chore: remove unused colorscheme

--- lua/alexfangsw/after/colorscheme.lua
+++ lua/alexfangsw/after/colorscheme.lua
@@ -1,28 +1,7 @@
-local function zenbones()
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme("zenbones")
-    -- colorcolumn and background color
-    vim.opt.termguicolors = true
-    vim.cmd("highlight ColorColumn guibg=#272725")
-    -- vim.cmd("highlight Normal guibg=#E0E0E0")
-end
-
 local function gruvbox()
     vim.g.gruvbox_material_background = 'hard'
     vim.g.gruvbox_material_better_performance = 1
     vim.cmd.colorscheme("gruvbox-material")
 end
 
-local function dracula()
-    vim.cmd.colorscheme("dracula")
-end
-
-local function vague()
-    require("vague").setup({})
-    vim.cmd.colorscheme("vague")
-end
-
--- dracula()
 gruvbox()
--- zenbones()
--- vague()
--- lua/alexfangsw/plugins/colorscheme.lua
+++ lua/alexfangsw/plugins/colorscheme.lua
@@ -2,21 +2,8 @@
 -- Better to move things in config function to 'after.colorscheme' folder
 -- =====================================================================
 
--- return {
--- 	"Mofiqul/dracula.nvim",
--- }
-
 return {
-	"sainnhe/gruvbox-material",
+	{
+		"sainnhe/gruvbox-material",
+	},
 }
-
--- return {
--- 	"zenbones-theme/zenbones.nvim",
--- 	dependencies = {
--- 		"rktjmp/lush.nvim",
--- 	},
--- }
-
--- return {
--- 	"vague2k/vague.nvim"
--- }
