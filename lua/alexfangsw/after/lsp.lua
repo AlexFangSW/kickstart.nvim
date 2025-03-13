25a2bd18918ceea1e177775b6939db1da866f3ca
AlexFangSW <alexfangsw@gmail.com>
2025-03-13 10:54:15 +0800

    chore: disable pyright

--- lua/alexfangsw/after/lsp.lua
+++ lua/alexfangsw/after/lsp.lua
@@ -39,18 +39,18 @@ require('mason-lspconfig').setup()
 
 local servers = {
     gopls = {},
-    pyright = {
-        pyright = {
-            -- Using Ruff's import organizer
-            disableOrganizeImports = true,
-        },
-        python = {
-            analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
-            },
-        },
-    },
+    -- pyright = {
+    --     pyright = {
+    --         -- Using Ruff's import organizer
+    --         disableOrganizeImports = true,
+    --     },
+    --     python = {
+    --         analysis = {
+    --             -- Ignore all files for analysis to exclusively use Ruff for linting
+    --             ignore = { '*' },
+    --         },
+    --     },
+    -- },
     ruff = {},
     jsonls = {},
     dockerls = {},
