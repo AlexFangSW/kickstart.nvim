5c5a92657240852861a3fd0f9a39a1b4df8ab58e
AlexFangSW <alexfangsw@gmail.com>
2025-04-07 08:48:49 +0800

    chore: enable pyright analysis

--- lua/alexfangsw/after/lsp.lua
+++ lua/alexfangsw/after/lsp.lua
@@ -48,7 +48,7 @@ local servers = {
         python = {
             analysis = {
                 -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
+                -- ignore = { '*' },
             },
         },
     },
@@ -157,3 +157,19 @@ mason_lspconfig.setup_handlers {
         lsp_setup_table[server_name](server_name)
     end,
 }
+
+-- https://docs.astral.sh/ruff/editors/setup/#neovim
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+            return
+        end
+        if client.name == 'ruff' then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+        end
+    end,
+    desc = 'LSP: Disable hover capability from Ruff',
+})
