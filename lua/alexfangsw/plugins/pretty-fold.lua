9394bece8420d49168cf546123a18826ab1dba80
AlexFangSW <alexfangsw@gmail.com>
2025-05-12 14:54:39 +0800

    fix: use pretty fold fork

--- lua/alexfangsw/plugins/pretty-fold.lua
+++ lua/alexfangsw/plugins/pretty-fold.lua
@@ -1,6 +1,7 @@
 return {
 	-- better fold
-	'anuvyklack/pretty-fold.nvim',
+	-- 'anuvyklack/pretty-fold.nvim',
+	'bbjornstad/pretty-fold.nvim',
 	config = function()
 		require('pretty-fold').setup({})
 	end
