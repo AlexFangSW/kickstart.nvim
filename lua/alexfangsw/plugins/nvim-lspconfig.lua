a62e084848fce5efcbf2acffff8295bcc110468d
AlexFangSW <alexfangsw@gmail.com>
2025-05-12 10:10:19 +0800

    feat: update lockfile

--- lua/alexfangsw/plugins/nvim-lspconfig.lua
+++ lua/alexfangsw/plugins/nvim-lspconfig.lua
@@ -3,8 +3,8 @@ return {
 	'neovim/nvim-lspconfig',
 	dependencies = {
 		-- Automatically install LSPs to stdpath for neovim
-		{ 'williamboman/mason.nvim', config = true },
-		'williamboman/mason-lspconfig.nvim',
+		{ 'williamboman/mason.nvim',           config = true },
+		{ 'williamboman/mason-lspconfig.nvim', version = "v1.32.0" },
 		'folke/neodev.nvim',
 	},
 }
