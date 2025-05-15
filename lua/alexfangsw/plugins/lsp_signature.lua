57629b8c323f4a7f57f66745d81319610dcc0a1e
AlexFangSW <alexfangsw@gmail.com>
2025-05-15 10:48:12 +0800

    chore: update lsp signature

--- lua/alexfangsw/plugins/lsp_signature.lua
+++ lua/alexfangsw/plugins/lsp_signature.lua
@@ -1,16 +1,11 @@
 -- return {
 -- 	"ray-x/lsp_signature.nvim",
--- 	event = "VeryLazy",
--- 	config = function()
--- 		-- [[ Configure lsp signature]]
--- 		local lsp_signature = require("lsp_signature")
--- 		lsp_signature.setup({
--- 			hint_enable = false
--- 		})
--- 	end
---
--- }
--- return {
--- 	"hrsh7th/cmp-nvim-lsp-signature-help"
+-- 	event = "InsertEnter",
+-- 	opts = {
+-- 		bind = true,
+-- 		handler_opts = {
+-- 			border = "rounded"
+-- 		}
+-- 	},
 -- }
 return {}
