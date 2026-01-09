b4701bc7bf35b783223b21ae3748d8bcd1acde92
AlexFangSW <alexfangsw@gmail.com>
2026-01-09 14:29:16 +0800

    chore: add go to code action

--- lua/alexfangsw/after/lsp.lua
+++ lua/alexfangsw/after/lsp.lua
@@ -16,6 +16,9 @@ local on_attach = function(_, bufnr)
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
     nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    -- Execute a code action, usually your cursor needs to be on top of an error
+    -- or a suggestion from your LSP for this to activate.
+    nmap('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction')
     -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
