81a0384e402be5bc43f75c3f528fd8ca42a57659
AlexFangSW <alexfangsw@gmail.com>
2026-01-09 13:56:44 +0800

    chore: adjust blink.cmp

--- lua/alexfangsw/plugins/blink.lua
+++ lua/alexfangsw/plugins/blink.lua
@@ -1,53 +1,45 @@
+-- Autocompletion
 return {
 	'saghen/blink.cmp',
-	-- optional: provides snippets for the snippet source
-	-- dependencies = { 'rafamadriz/friendly-snippets' },
-
-	-- use a release tag to download pre-built binaries
+	event = 'VimEnter',
 	version = '1.*',
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
+	dependencies = {
+		'folke/lazydev.nvim',
+	},
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = 'default' },
+		keymap = {
+			-- 'default' (recommended) for mappings similar to built-in completions
+			--   <c-y> to accept ([y]es) the completion.
+			--    This will auto-import if your LSP supports it.
+			--    This will expand snippets if the LSP sent a snippet.
+			-- 'super-tab' for tab to accept
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			preset = 'default',
+		},
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = 'mono'
+			nerd_font_variant = 'mono',
 		},
 
-		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
+		completion = {
+			-- By default, you may press `<c-space>` to show the documentation.
+			-- Optionally, set `auto_show = true` to show the documentation after a delay.
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { 'lsp', 'path', 'snippets', 'lazydev' },
+			providers = {
+				lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+			},
 		},
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "prefer_rust_with_warning" }
+		fuzzy = { implementation = 'prefer_rust_with_warning' },
+		signature = { enabled = true },
 	},
-	opts_extend = { "sources.default" }
 }
